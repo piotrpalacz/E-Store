8e58feb15da0d3464cb6c7580528e0e1dde4aa88
Piotr Palacz <peterpalacz12@gmail.com>
2023-06-04 21:29:42 +0200

    Section 3: lesson 35

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -26,6 +26,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(opt => 
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,39 @@
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { Product } from "../../app/models/product";
+
+interface Props {
+  product: Product;
+}
+
+export default function ProductCard({product}: Props) {
+  return (
+    <Card>
+      <CardHeader 
+        avatar={
+          <Avatar>
+            {product.name.charAt(0).toUpperCase()}
+          </Avatar>
+        }
+        title={product.name}
+
+      />
+      <CardMedia
+        sx={{ height: 140 }}
+        image={product.pictureUrl}
+        title={product.name}
+      />
+      <CardContent>
+        <Typography gutterBottom color='secondary' variant="h5" component="div">
+          {product.price}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {product.brand} / {product.type}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add to cart</Button>
+        <Button size="small">View</Button>
+      </CardActions>
+    </Card>
+  );
+}
