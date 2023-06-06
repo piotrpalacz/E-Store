3355845e9f81e8969ff09f1baa72cdc2ee54f9e5
Piotr Palacz <peterpalacz12@gmail.com>
2023-06-06 23:37:51 +0200

    feature: switch dark/light mode button

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,43 +1,33 @@
-import { useEffect, useState } from "react";
-import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Container, CssBaseline } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "./Header";
+import { useState } from "react";
 
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false)
+  const palleteType = darkMode ? 'dark' : 'light'
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: palleteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data));
-  }, []) 
-
-  function addProduct() {
-    setProducts(prevState => [...prevState, 
-      {
-        id: prevState.length + 101,
-        name: 'product' + (prevState.length + 1), 
-        description: "Hello what",
-        price: (prevState.length * 100) + 100,
-        brand: 'some brand',
-        pictureUrl: 'http://picsum.photos/200',
-        type: 'nothing',
-        // quantityInStock: 0
-      }])
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
   }
 
   return (
-    <>
-    <CssBaseline />
-      <Header />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog products={products} addProduct={addProduct}/>
+        <Catalog />
       </Container>
-      
-      
-    </>
+    </ThemeProvider>
   );
 }
 
